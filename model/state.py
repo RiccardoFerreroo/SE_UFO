09a4904d734cd5ccfdebb8941bdcf9888a1f72b6
RiccardoFerreroo <s336535@studenti.polito.it>
2026-06-22 23:52:00 +0200

    my

---
+++ model/state.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+@dataclass
+class State:
+    id : str
+    name: str
+    capital: str
+    lat: float
+    lng: float
+    area: float
+    population:int
+    neighbors: str
+
+
+
+    def __str__(self):
+        return self.name
+    def __repr__(self):
+        return self.name
+    def __hash__(self):
+        return hash(self.id)
+    def __eq__(self, other):
+        return self.id == other.id
+
