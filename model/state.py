453927163af8a4c6bc35249aef4be99ac9b02324
RiccardoFerreroo <s336535@studenti.polito.it>
2026-09-06 10:27:32 +0200

    my ufo

--- model/state.py
+++ model/state.py
@@ -1,24 +1,18 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass
 class State:
-    id : str
+    id: int
     name: str
     capital: str
     lat: float
     lng: float
     area: float
-    population:int
-    neighbors: str
-
-
+    population: int
+    neighbors: list = field(default_factory=list)
 
     def __str__(self):
         return self.name
-    def __repr__(self):
-        return self.name
+
     def __hash__(self):
         return hash(self.id)
-    def __eq__(self, other):
-        return self.id == other.id
-
